deed98f7903cebc34defbca944f140031ccf4031
ourfbht <ourfbht@gmail.com>
2020-04-22 19:49:09 +0200

    validator for HelpProposition done

---
+++ src/main/java/com/example/demo/validator/HelpPropositionValidator.java
@@ -0,0 +1,31 @@
+package com.example.demo.validator;
+
+import com.example.demo.model.HelpProposition;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class HelpPropositionValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return HelpProposition.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        HelpProposition helpProposition = (HelpProposition) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "branch", "branch.empty", "You must enter a branch!");
+        if(helpProposition.getBranch().length() < 2 || helpProposition.getBranch().length() > 32){
+            errors.rejectValue("branch", "branch.size", "The size must be between 2 and 32");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "timeBegin", "timeBegin.empty", "You must enter a time begin!");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "timeEnd", "timeEnd.empty", "You must enter a time end!");
+    }
+
+}
